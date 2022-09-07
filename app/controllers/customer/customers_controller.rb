d9580101b2eab8438a42768ad2458b63fd744cbc
gon0131 <keigorichan@icloud.com>
2022-09-07 18:37:35 +0000

    Initial Commit

---
+++ app/controllers/customer/customers_controller.rb
@@ -0,0 +1,16 @@
+class Customer::CustomersController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def deactivate
+  end
+  
+  def withdraw
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,33 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  devise_for :customers,skip: [:passwords,], controllers: {
+    registrations: "customer/registrations",
+    sessions: 'customer/sessions'
+  }
+
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  namespace :admin do
+    get "/" => "homes#top"
+    resources :orders
+    resources :customers
+    resources :genres
+    resources :items
+  end
+  
+  scope module: :customer do
+    root to: "homes#top"
+    get "/about" => "homes#about"
+    get "/customers/my_page" => "customers#show"
+    get "/customers/information/edit" => "customers#edit"
+    patch "/customers/information" => "customers#update"
+    get "/customers/deactivate" => "customers#deactivate"
+    patch "/customers/withdraw" => "customers#withdraw"
+    resources :items
+    resources :cart_items
+    resources :orders
+    resources :addresses
+  end
 end
