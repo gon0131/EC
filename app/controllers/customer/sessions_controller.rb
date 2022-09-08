f426c09e0af87033c12c2dd9e3da2815967ee904
gon0131 <keigorichan@icloud.com>
2022-09-08 14:25:54 +0000

    Initial Commit

--- app/controllers/customer/sessions_controller.rb
+++ app/controllers/customer/sessions_controller.rb
@@ -3,6 +3,7 @@
 class Customer::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :customer_state, only: [:create]
 
   def after_sign_in_path_for(resource)
     customers_my_page_path
@@ -17,25 +18,13 @@ class Customer::SessionsController < Devise::SessionsController
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :phone_number, :address])
   end
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  
+  def customer_state
+    @customer = Customer.find_by(email: params[:customer][:email])
+    return if !@customer
+    if @customer.valid_password?(params[:customer][:password]) && @customer.is_deleted == true
+      redirect_to new_customer_registration_path
+    end
+  end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
