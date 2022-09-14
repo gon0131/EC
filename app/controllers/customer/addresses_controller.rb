169ad05e1fd1819c8f0b3bf9f7f53164bccca732
gon0131 <keigorichan@icloud.com>
2022-09-14 05:25:26 +0000

    Initial Commit

--- app/controllers/customer/addresses_controller.rb
+++ app/controllers/customer/addresses_controller.rb
@@ -1,16 +1,41 @@
 class Customer::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = current_customer.addresses
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
   
   def create
+    @address = Address.new(address_params)
+    if @address.save
+      redirect_to addresses_path
+    else
+      @addresses =current_customer.addresses
+      render :index
+    end
   end
   
   def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render :edit
+    end
   end
   
   def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path
+  end
+  
+  private
+  
+  def address_params
+    params.require(:address).permit(:post_code, :address, :name, :customer_id)
   end
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,11 +1,8 @@
 class CartItem < ApplicationRecord
   belongs_to :item
   belongs_to :customer
+  validates :amount, presence: true
   def subtotal
     item.with_tax_price * amount
-  end
-  
-  def total
-    
   end
 end
